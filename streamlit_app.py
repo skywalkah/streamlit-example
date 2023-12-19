ed49a79406e1cf0060b20efea927c1c2e536d01e
skywalkah <lucas.schvindt@gmail.com>
2023-12-19 13:11:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,6 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache_data)")
+
+st.subheader('Raw data')
+st.write(data)
